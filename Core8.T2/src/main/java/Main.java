ebc4718268f93698fd72679647657ae2c1c4c46e
Dima <bodryi2001@icloud.com>
2024-05-22 15:54:44 +0300

    First commit

---
+++ Core8.T2/src/main/java/Main.java
@@ -0,0 +1,51 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Main {
+
+    public static final String REMOTE_SERVICE_URI = "https://api.nasa.gov/planetary/apod?api_key=A5Q7BbJPQ0E3gyiNyUybdeAmQ6mHijJIAzfuI7TD";
+
+    public static void main(String[] args) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create()
+                .setDefaultRequestConfig(
+                        RequestConfig.custom()
+                                .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
+                                .setSocketTimeout(30000)    // максимальное время ожидания получения данных
+                                .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
+                                .build())
+                .build();
+
+
+        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse response = httpClient.execute(request);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        NASAContent content = objectMapper.readValue(
+                response.getEntity().getContent(),
+                NASAContent.class
+        );
+
+        HttpGet requestToNASA = new HttpGet(content.getUrl());
+        requestToNASA.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+        CloseableHttpResponse responseFromNASA = httpClient.execute(requestToNASA);
+
+        var file = new File(content.getUrl().substring(content.getUrl().lastIndexOf("/") + 1));
+        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+            outputStream.write(responseFromNASA.getEntity().getContent().readAllBytes());
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
