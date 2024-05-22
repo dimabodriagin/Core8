0fdb9a0fbe4d3f46693e5eb832b243fecf01877a
Dima <bodryi2001@icloud.com>
2024-05-22 18:10:35 +0300

    Add try-with-resources and close method. Refactor NASAContent with camelCase

--- Core8.T2/src/main/java/Main.java
+++ Core8.T2/src/main/java/Main.java
@@ -15,37 +15,43 @@ public class Main {
 
     public static final String REMOTE_SERVICE_URI = "https://api.nasa.gov/planetary/apod?api_key=A5Q7BbJPQ0E3gyiNyUybdeAmQ6mHijJIAzfuI7TD";
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(
                         RequestConfig.custom()
                                 .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
                                 .setSocketTimeout(30000)    // максимальное время ожидания получения данных
                                 .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
                                 .build())
-                .build();
+                .build()) {
 
 
-        HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
-        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
-        CloseableHttpResponse response = httpClient.execute(request);
+            HttpGet request = new HttpGet(REMOTE_SERVICE_URI);
+            request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            CloseableHttpResponse response = httpClient.execute(request);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        NASAContent content = objectMapper.readValue(
-                response.getEntity().getContent(),
-                NASAContent.class
-        );
+            ObjectMapper objectMapper = new ObjectMapper();
+            NASAContent content = objectMapper.readValue(
+                    response.getEntity().getContent(),
+                    NASAContent.class
+            );
 
-        HttpGet requestToNASA = new HttpGet(content.getUrl());
-        requestToNASA.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
-        CloseableHttpResponse responseFromNASA = httpClient.execute(requestToNASA);
+            HttpGet requestToNASA = new HttpGet(content.getUrl());
+            requestToNASA.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            CloseableHttpResponse responseFromNASA = httpClient.execute(requestToNASA);
 
-        var file = new File(content.getUrl().substring(content.getUrl().lastIndexOf("/") + 1));
-        try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
-            outputStream.write(responseFromNASA.getEntity().getContent().readAllBytes());
+            var file = new File(content.getUrl().substring(content.getUrl().lastIndexOf("/") + 1));
+            try (FileOutputStream outputStream = new FileOutputStream(file, false)) {
+                outputStream.write(responseFromNASA.getEntity().getContent().readAllBytes());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+            response.close();
+            responseFromNASA.close();
         } catch (IOException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,28 +13,34 @@ import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        CloseableHttpClient httpClient = HttpClientBuilder.create()
+    public static void main(String[] args){
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create()
                 .setDefaultRequestConfig(
                         RequestConfig.custom()
                                 .setConnectTimeout(5000)    // максимальное время ожидание подключения к серверу
                                 .setSocketTimeout(30000)    // максимальное время ожидания получения данных
                                 .setRedirectsEnabled(false) // возможность следовать редиректу в ответе
                                 .build())
-                .build();
+                .build()) {
 
-        HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
-        request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
-        CloseableHttpResponse response = httpClient.execute(request);
+            HttpGet request = new HttpGet("https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats");
+            request.setHeader(HttpHeaders.ACCEPT, ContentType.APPLICATION_JSON.getMimeType());
+            CloseableHttpResponse response = httpClient.execute(request);
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<CatsInfo> catsInfos = objectMapper.readValue(
-                response.getEntity().getContent(),
-                new TypeReference<List<CatsInfo>>() {}
-        );
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<CatsInfo> catsInfos = objectMapper.readValue(
+                    response.getEntity().getContent(),
+                    new TypeReference<List<CatsInfo>>() {
+                    }
+            );
 
-        catsInfos.stream()
-                .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
-                .forEach(System.out::println);
+            catsInfos.stream()
+                    .filter(value -> value.getUpvotes() != null && value.getUpvotes() > 0)
+                    .forEach(System.out::println);
+
+            response.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
